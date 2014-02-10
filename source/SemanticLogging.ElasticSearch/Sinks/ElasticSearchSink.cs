753e3a3465d38d2ca2470f7fdfe3de768c57e77f
noocyte <jarle.nygaard@outlook.com>
2014-02-10 14:57:31 +0100

    Beginning to add ES sink

---
+++ source/SemanticLogging.ElasticSearch/Sinks/ElasticSearchSink.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Sinks
+{
+    public class ElasticSearchSink : IObserver<JsonEventEntry>, IDisposable
+    {
+        public void OnNext(JsonEventEntry value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
