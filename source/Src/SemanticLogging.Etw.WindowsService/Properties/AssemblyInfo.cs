e000eb10aa6a4319a6828c0f23f6ce73c6c63756
fsimonazzi <fsimonazzi@hotmail.com>
2014-06-24 11:00:02 -0300

    update version numbers

--- source/Src/SemanticLogging.Etw.WindowsService/Properties/AssemblyInfo.cs
+++ source/Src/SemanticLogging.Etw.WindowsService/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Enterprise Library Semantic Logging Service")]
 [assembly: AssemblyDescription("Enterprise Library Semantic Logging Service")]
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.1403.0")]
-[assembly: AssemblyInformationalVersion("1.1.1403.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.1406.0")]
+[assembly: AssemblyInformationalVersion("2.0.1406-rc")]
 
 [assembly: ComVisible(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
