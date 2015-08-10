484ea3c2999b66acaad957675cb2682128c07a2c
Julius Liu <juliusl@microsoft.com>
2015-08-10 16:04:48 -0700

    Update elk-simple-install-ubuntu.sh
    fixed broken links

--- ELK/AzureRM/elk-simple-on-ubuntu/elk-simple-install-ubuntu.sh
+++ ELK/AzureRM/elk-simple-on-ubuntu/elk-simple-install-ubuntu.sh
@@ -63,8 +63,8 @@ done
 mkdir /opt/elk-simple/
 cd /opt/elk-simple/
 wget https://raw.githubusercontent.com/juliusl/azure-quickstart-templates/master/elasticsearch/elasticsearch-ubuntu-install.sh
-wget https://raw.githubusercontent.com/mspnp/semantic-logging/v3/ELK/AzureRM/logstash-on-ubuntu/logstash-install-ubuntu.sh
-wget https://raw.githubusercontent.com/mspnp/semantic-logging/v3/ELK/AzureRM/elk-simple-on-ubuntu/kibana4-install-ubuntu.sh
+wget https://raw.githubusercontent.com/mspnp/semantic-logging/elk/ELK/AzureRM/logstash-on-ubuntu/logstash-install-ubuntu.sh
+wget https://raw.githubusercontent.com/mspnp/semantic-logging/elk/ELK/AzureRM/elk-simple-on-ubuntu/kibana4-install-ubuntu.sh
 
 #Install ELK
 log "Installing Elasticsearch" 
